f1d66f054f4ad948865f83ff66a43ffc71137564
Siwoo <postsw7@gmail.com>
2017-06-25 16:47:23 +0900

    fix anti patten

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ class App extends Component {
     this.state = {
       items: shopItems.items
     }
+
   }
 
-  onClickTitle = (e) => {
+  onClickTitle (e) {
     if (e.target.className.includes('-o')) {
       e.target.classList.remove('fa-check-circle-o');
       e.target.classList.add('fa-check-circle');
@@ -23,30 +24,56 @@ class App extends Component {
     }
   }
 
-  onClickNav = (e) => {
-    let navList = document.getElementsByTagName('LI');
+  onClickNav (e) {
+    let navList = e.currentTarget.children;
     Array.prototype.forEach.call(navList, navItem => {
       if (navItem.className.includes('select-nav')) {
         navItem.classList.remove('select-nav');
       }
     })
     e.target.classList.add('select-nav');
+    if (e.target.innerHTML === '인기순') {
+
+      let items = shopItems.items.slice();
+      this.setState({
+        items: items.sort((prev, item) => {
+          return item.purchase - prev.purchase;
+        })
+      })
+    } else if (e.target.innerHTML === '저가순') {
+      let items = shopItems.items.slice();
+      this.setState({
+        items: items.sort((prev, item) => {
+          return +prev.price.split(',').join('') - +item.price.split(',').join('');
+        })
+      })
+    } else if (e.target.innerHTML === '신상품') {
+      let items = shopItems.items.slice();
+      this.setState({
+        items: items.sort((prev, item) => {
+          return prev.id - item.id;
+        })
+      })
+    }
   }
 
-  handleSearch = (e, searchResult) => {
+  handleEnterSearch (e) {
     if (e.key === 'Enter') {
       this.setState({
         items: shopItems.items.filter(item => {
           return item.title.includes(e.target.value);
         })
       });
-    } else if (searchResult) {
-      this.setState({
-        items: shopItems.items.filter(item => {
-          return item.title.includes(searchResult);
-        })
-      });
     }
+  }
+
+  handleClickSearch (e) {
+    let value = e.target.nextElementSibling.value;
+    this.setState({
+      items: shopItems.items.filter(item => {
+        return item.title.includes(value);
+      })
+    });
   }
 
   render () {
@@ -55,25 +82,25 @@ class App extends Component {
         <header>
           <div className="container">
             <div className="title">
-              <h1 lang="ko">바이슬림</h1>
-              <span className="fa fa-check-circle-o fa-lg" aria-hidden="true" onClick={(e) => { this.onClickTitle(e); }}></span>
+              <h1>바이슬림</h1>
+              <span className="fa fa-check-circle-o fa-lg" aria-hidden="true" onClick={this.onClickTitle.bind(this)}></span>
             </div>
             <nav>
-              <ul lang="ko" onClick={(e) => { this.onClickNav(e); }}>
+              <ul onClick={this.onClickNav.bind(this)}>
                 <li className="select-nav">신상품</li>
                 <li>인기순</li>
                 <li>저가순</li>
               </ul>
               <div className="search-item">
-                <span className="fa fa-search" aria-hidden="true" onClick={(e) => { this.handleSearch(e, e.target.nextElementSibling.value); }}></span>
-                <input type="text" placeholder="상품검색" onKeyPress={(e) => { this.handleSearch(e); }}></input>
+                <span className="fa fa-search" aria-hidden="true" onClick={this.handleClickSearch.bind(this)}></span>
+                <input type="text" placeholder="상품검색" onKeyPress={this.handleEnterSearch.bind(this)}></input>
               </div>
             </nav>
           </div>
         </header>
         <main>
           <div className="container">
-            <ShopItemList itemList={this.state.items}/>
+            <ShopItemList itemList={this.state.items} />
           </div>
         </main>
       </div>
